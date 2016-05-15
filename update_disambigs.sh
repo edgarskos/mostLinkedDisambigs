1517b93a4b6da0caae72ead946f82138831b8a6a
Martin Urbanec <martin@urbanec.cz>
2016-05-15 10:48:31 +0200

    Remove old files

--- update_disambigs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./script2.py
-./script3.py
-python get.py Wikipedista:UrbanecmBot/DisambigLinks | sed -e s/odkazu/odkazů/g > out
-./script4.py
-python ~/pywikibot/scripts/add_text.py -text:"$(cat out)" -page:"Wikipedista:UrbanecmBot/DisambigLinks"
-rm out
