662b8c430c3f1ae2a75eba8a463d9a36638e6409
Martin Urbanec <martin@urbanec.cz>
2016-06-12 16:45:05 +0200

    New version

--- mostLinkedDisambigs.py
+++ mostLinkedDisambigs.py
@@ -8,7 +8,7 @@ conn = db.connect('cswiki')
 
 cur = conn.cursor()
 with cur:
-	sql = 'select count(*), pl_title from pagelinks where pl_title in (select page_title from page where page_id in (select cl_from from categorylinks where cl_to="Rozcestníky")) group by pl_title'
+	sql = 'select count(*), pl_title from pagelinks where pl_title in (select page_title from page where pl_from_namespace=0 and page_id in (select cl_from from categorylinks where cl_to="Rozcestníky")) group by pl_title'
 	cur.execute(sql)
 	data = cur.fetchall()
 
